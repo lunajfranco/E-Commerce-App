d7af58e4ab4f1ecdce0da4986eddfe491e793783
Franco Luna <lunajfranco@outlook.com>
2020-11-04 22:43:00 -0300

    Commit Inicial

---
+++ BethanysPieShop/Models/IPieRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> AllPies { get; }
+        IEnumerable<Pie> PiesOfTheWeek { get; }
+        Pie GetPieById(int pieId);
+    }
+}
---
+++ BethanysPieShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BethanysPieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public HomeViewModel(IEnumerable<Pie> piesOfTheWeek)
+        {
+            PiesOfTheWeek = piesOfTheWeek;
+        }
+        public IEnumerable<Pie> PiesOfTheWeek { get; set; }
+    }
+}
